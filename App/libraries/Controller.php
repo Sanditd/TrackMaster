a18bb63d2148c1885e866e74413e8a8b905e864a
Sandith Moras <sandithdev@gmail.com>
2024-11-17 19:32:17 +0530

    resolve conflicts

---
+++ App/libraries/Controller.php
@@ -0,0 +1,22 @@
+<?php
+    class controller{
+        //To Load the Model
+        public function model($model){
+            require_once '../model/'. $model. '.php';
+            //Instantiate the model and pass it to the controller member variable
+            return new $model();
+        }
+
+    //To Load the View
+    public function view($view, $data = []) {
+        $path = __DIR__ . '/../views/Admin/' . $view . '.php';
+        if (file_exists($path)) {
+            require_once $path;
+        } else {
+            die("Corresponding View does not exist: $path");
+        }
+    }
+    
+    }
+
+?>
