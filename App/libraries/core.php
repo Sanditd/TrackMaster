97a0732bb95eaa1d7a165e43aae56bc4ed703424
Sandith Moras <sandithdev@gmail.com>
2024-11-20 12:36:59 +0530

    Coach signup added

--- App/libraries/core.php
+++ App/libraries/core.php
@@ -1,50 +1,35 @@
 <?php
-    class Core{
-        //URL format --> controller/methods/parameters
+class Core {
+    protected $currentController = 'Student';
+    protected $currentMethod = 'index';
+    protected $params = [];
 
-        protected $currentController = 'student';
-        protected $currentMethod = 'index';
-        protected $params = [];
+    public function __construct() {
+        $url = $this->getUrl();
 
-        public function __construct(){
-            $url = $this->getUrl();
-
-            if(file_exists('../app/controllers/'.ucwords($url[0]).'.php')){
-                //if the controller exists then load it
-                $this->currentController = ucwords($url[0]);
-                //unset the controller in the URL
-                unset($url[0]);
-                //call the controller
-                require_once '../app/controllers/'.$this->currentController.'.php';
-                //Instantiate the controller
-                $this->currentController = new $this->currentController;
-
-                //check whether the method exists in the controller
-                if(isset($url[1])){
-                    if(method_exists($this->currentController, $url[1])){
-                        //if the method exists then load it
-                        $this->currentMethod = $url[1];
-                        //unset the method in the URL
-                        unset($url[1]);
-                    }
-                }
-
-                //get parameter list
-                $this->params = $url? array_values($url) : [];
-                //call the method and pass parameters list
-                call_user_func_array([$this->currentController,$this->currentMethod],$this->params);
-            }
+        if ($url && file_exists('../app/controllers/' . ucwords($url[0]) . '.php')) {
+            $this->currentController = ucwords($url[0]);
+            unset($url[0]);
         }
 
-        public function getUrl(){
-            if(isset($_GET['url'])){
-                $url = rtrim($_GET['url'], '/');
-                $url = filter_var($url, FILTER_SANITIZE_URL);
-                $url = explode('/', $url);
+        require_once '../app/controllers/' . $this->currentController . '.php';
+        $this->currentController = new $this->currentController;
 
-                return $url;
-            }
+        if (isset($url[1]) && method_exists($this->currentController, $url[1])) {
+            $this->currentMethod = $url[1];
+            unset($url[1]);
         }
+
+        $this->params = $url ? array_values($url) : [];
+        call_user_func_array([$this->currentController, $this->currentMethod], $this->params);
     }
 
+    public function getUrl() {
+        if (isset($_GET['url'])) {
+            $url = rtrim($_GET['url'], '/');
+            $url = filter_var($url, FILTER_SANITIZE_URL);
+            return explode('/', $url);
+        }
+    }
+}
 ?>
