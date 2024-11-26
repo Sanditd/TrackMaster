fefa891e9f072a9953f0a733ef84f6de80e92d90
Eraji Thenuwara <thenuwaraeraji@gmail.com>
2024-11-26 14:09:33 +0530

    edited

--- App/libraries/controller.php
+++ App/libraries/controller.php
@@ -2,14 +2,19 @@
     class controller{
         //To Load the Model
         public function model($model){
-            require_once '../model/'. $model. '.php';
-            //Instantiate the model and pass it to the controller member variable
-            return new $model();
+            $path=__DIR__ .'/../model/'. $model. '.php';
+            if (file_exists($path)) {
+            require_once $path;
+             //Instantiate the model and pass it to the controller member variable
+             return new $model();
+            } else {
+                die("Corresponding View does not exist: $path");
+             }
         }
 
     //To Load the View
     public function view($view, $data = []) {
-        $path = __DIR__ . '/../views/Admin/' . $view . '.php';
+        $path = __DIR__ . '/../views/' . $view . '.php';
         if (file_exists($path)) {
             require_once $path;
         } else {
