e81ac25aea16ac3f96f03e359a8c8acb9052adb1
Sandith Moras <sandithdev@gmail.com>
2024-11-28 12:37:50 +0530

    resolve conflicts

--- App/libraries/controller.php
+++ App/libraries/controller.php
@@ -2,16 +2,20 @@
     class controller{
         //To Load the Model
         public function model($model){
-            require_once $path=__DIR__ .'/../model/'. $model. '.php';
-            //Instantiate the model and pass it to the controller member variable
-            return new $model();
+            $path=__DIR__ .'/../model/'. $model. '.php';
+            if (file_exists($path)) {
+            require_once $path;
+             //Instantiate the model and pass it to the controller member variable
+             return new $model();
+            } else {
+                die("Corresponding View does not exist: $path");
+             }
         }
 
     //To Load the View
     public function view($view, $data = []) {
-        $path = __DIR__ . '/../views/' . $view . '.php';
+        $path = __DIR__ . '/../views/Admin/' . $view . '.php';
         if (file_exists($path)) {
-            extract($data);
             require_once $path;
         } else {
             die("Corresponding View does not exist: $path");
