15b1ac41ed068a1645e8789cbf475d3dc91796cc
Eraji Thenuwara <thenuwaraeraji@gmail.com>
2024-11-29 09:29:32 +0530

    coach nav bar edited

--- App/views/Coach/CoachNav.php
+++ App/views/Coach/CoachNav.php
@@ -1,10 +1,20 @@
 <div id="sidebar" class="sidebar">
         <span class="close" onmouseleave="closeNav()">
         <div class="profile">
-            <img src="../Public/img/Student/practicing.png" alt="Profile Picture">
+            <img src="/TrackMaster/Public/img/profile.jpeg" alt="Profile Picture">
             <div style="font-size:15px;font-weight:bold;white-space: nowrap;">T.H.E.G.THENUWARA</div>
             <br>
             <div style="font-size:10px;white-space: nowrap;">Coach - Cricket</div>
+            <br>
+            <div>
+                <select id="level">
+                  <option value="zonal">Zonal</option>
+                  <option value="provincial">Provincial</option>
+                  <option value="national">National</option>
+                </select>
+              </div>
+            <br>
+            <hr>
         </div>
 
         <nav>
@@ -21,22 +31,24 @@
     </div>
 
     <div id="main">
-        <header>
-            <button class="openbtn" onclick="toggleNav()">☰ </button>
+    <header>
+        <button class="openbtn" onclick="toggleNav()">☰ </button>
 
-            <nav class="navbar" id="navbar">
-                <div class="logo">
-                    <img src="../Public/img/logo.png" alt="Logo"> 
-                </div>
-                <ul class="nav-links">
-                    <li><a href="../Student/dashboard.html"><i class="fas fa-home"></i> Home</a></li>
-                    <li><a href="#">About Us</a></li>
-                    <li><a href="#">Services</a></li>
-                    <li><a href="#">Contact</a></li>
-                    <li><a href="#">Sign In</a></li>
-                    <li><a href="#">Log In</a></li>
-                </ul>
-            </nav>
-
-        </header>
+        <nav class="navbar" id="navbar">
+            <div class="logo">
+                <img src="/TrackMaster/Public/img/logo.png" alt="Logo"> 
+            </div>
+            <ul class="nav-links">
+                <li><a href="<?php echo URLROOT ?>/home">Home</a></li>
+                <li><a href="<?php echo URLROOT ?>/aboutus">About Us</a></li>
+                <li><a href="<?php echo URLROOT ?>/help">Help & Support</a></li>    
+                <li><button class="login" onclick="window.location.href='<?php echo URLROOT ?>/login'">Log in</button></li>
+                <li><img src="/TrackMaster/Public/img/log in.png" alt="Logo"> </li>
+                <li><img src="/TrackMaster/Public/img/notification.png" alt="Logo"> </li>
+            </ul>
+        </nav>
+    </header>
     </div>
+
+    <script src="/TrackMaster/Public/js/sidebar.js"></script>
+    <script src="/TrackMaster/Public/js/coachlevel.js"></script>
